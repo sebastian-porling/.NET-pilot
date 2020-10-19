9d5da3cad07a0e6186b9288b4d6494adc82e951a
Sebastian Porling <sebastian.porling@gmail.com>
2020-10-19 14:17:49 +0200

    feat: first console app

---
+++ FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FirstConsoleApp
+{
+    /// <summary>
+    /// A simple Console Application that prints out a greeting
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Main entry for a console application that creates a greeting based on user input
+        /// </summary>
+        /// <param name="args">Arguments ignored</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Anon! Enter your name:");
+            string name;
+            while ((name = Console.ReadLine()).Length < 1) { }
+            Console.WriteLine($"Hello {name}!, " +
+                $"your name is {name.Replace(" ", "").Length} " +
+                $"characters long and starts with a {name.Substring(0, 1)}");
+            
+        }
+    }
+}
