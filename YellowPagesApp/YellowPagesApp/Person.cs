55b4ac713f833804ef42f09899f94be5b671d8da
Sebastian Porling <sebastian.porling@gmail.com>
2020-10-21 07:21:19 +0200

    feat: Yellow Pages assignment finnished

---
+++ YellowPagesApp/YellowPagesApp/Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace YellowPagesApp
+{
+    /// <summary>
+    /// Represents a person with first and lastname
+    /// </summary>
+    class Person
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string FullName { get => $"{FirstName} {LastName}"; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="firstName">String, persons firstname</param>
+        /// <param name="lastName">String, persons lastname</param>
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        /// <summary>
+        /// Checks if query is equal to the fullname
+        /// </summary>
+        /// <param name="query">String, search query</param>
+        /// <returns>True if full match</returns>
+        public bool FullMatch(string query)
+        {
+            return FullName.ToLower().Equals(query.ToLower());
+        }
+
+        /// <summary>
+        /// Checks if query matches the fullname
+        /// </summary>
+        /// <param name="query">String, search query</param>
+        /// <returns>True if query matches the name in any way</returns>
+        public bool PartialMatch(string query)
+        {
+            return FullName.ToLower().Contains(query.ToLower());
+        }
+
+        /// <summary>
+        /// String representation of the person class
+        /// </summary>
+        /// <returns>Fullname of the person</returns>
+        public override string ToString()
+        {
+            return FullName;
+        }
+    }
+}
---
+++ YellowPagesApp/YellowPagesApp/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YellowPagesApp
+{
+    /// <summary>
+    /// The Yellow Page application that can search through a catalog of persons
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Main program,
+        /// Takes in search query from user and prints the full matches and partial matches
+        /// </summary>
+        /// <param name="args">Arguments ignored</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a search query (full name or part of name): ");
+            string query = Console.ReadLine();
+            List<Person> persons = getYellowPages();
+            Console.WriteLine("\nFull Matches: ");
+            getFullMatches(query, persons).ForEach(Console.WriteLine);
+            Console.WriteLine("\nPartial Matches: ");
+            getPartialMatches(query, persons).ForEach(Console.WriteLine);
+        }
+
+        /// <summary>
+        /// Generates five persons in a person list
+        /// </summary>
+        /// <returns>A person list</returns>
+        static List<Person> getYellowPages()
+        {
+            List < Person > yellowPages = new List<Person>();
+            yellowPages.Add(new Person("Sean", "Skinner"));
+            yellowPages.Add(new Person("Sebastian", "Porling"));
+            yellowPages.Add(new Person("James", "Bond"));
+            yellowPages.Add(new Person("Sean", "Connor"));
+            yellowPages.Add(new Person("David", "Stjernqvist"));
+            return yellowPages;
+        }
+
+        /// <summary>
+        /// Returns all persons that are equal to the search query
+        /// </summary>
+        /// <param name="query">Search query</param>
+        /// <param name="persons">Lis tof persons</param>
+        /// <returns>List of persons that are equal to the search query</returns>
+        static List<Person> getFullMatches(string query, List<Person> persons)
+        {
+            return persons.FindAll(person => person.FullMatch(query));
+        }
+
+        /// <summary>
+        /// Returns all persons that matches the search query
+        /// </summary>
+        /// <param name="query">Search query</param>
+        /// <param name="persons">List of persons</param>
+        /// <returns>List of persons that matches the search query</returns>
+        static List<Person> getPartialMatches(string query, List<Person> persons)
+        {
+            return persons.FindAll(person => person.PartialMatch(query));
+        }
+    }
+}
